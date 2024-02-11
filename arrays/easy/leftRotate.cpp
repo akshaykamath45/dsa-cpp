b020782ca6ecc06e13bd21d40ac3c1fc1f4b1cfe
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2024-02-12 00:02:40 +0530

    left rotate array by one place
    https://www.codingninjas.com/codestudio/problems/left-rotate-an-array-by-one_5026278

---
+++ arrays/easy/leftRotate.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+vector<int> rotateArray(vector<int> &arr, int n)
+{
+    int temp = arr[0];
+    for (int i = 1; i < n; i++)
+    {
+        arr[i - 1] = arr[i];
+    }
+    // for (int i = 0; i < n-1; i++)
+    // {
+    //     arr[i] = arr[i+1];
+    // }
+    arr[n - 1] = temp;
+    return arr;
+}
