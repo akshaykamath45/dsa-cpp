e7aaa959e7097f892cdac75689bf276ec41e73c9
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2024-03-25 15:33:02 +0530

    first and last occurence of element in a sorted array

--- binary search/bs on 1d arrays/firstAndlastOccurence.cpp
+++ binary search/bs on 1d arrays/firstAndlastOccurence.cpp
@@ -1,3 +1,4 @@
+// using upper and lower bound
 class Solution
 {
 public:
@@ -55,3 +56,68 @@ public:
         return {lb, ub - 1};
     }
 };
+
+// Using binary search directly
+int firstOccurence(vector<int> &arr, int x)
+{
+    int n = arr.size();
+    int low = 0;
+    int high = n - 1;
+    int first = -1;
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+        if (arr[mid] == x)
+        {
+            first = mid;
+            high = mid - 1;
+        }
+        else if (arr[mid] < x)
+        {
+            low = mid + 1;
+        }
+        else
+        {
+            high = mid - 1;
+        }
+    }
+    return first;
+}
+
+int lastOccurence(vector<int> &arr, int x)
+{
+    int n = arr.size();
+    int low = 0;
+    int high = n - 1;
+    int last = -1;
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+        if (arr[mid] == x)
+        {
+            last = mid;
+            low = mid + 1;
+        }
+        else if (arr[mid] < x)
+        {
+            low = mid + 1;
+        }
+        else
+        {
+            high = mid - 1;
+        }
+    }
+    return last;
+}
+
+pair<int, int> firstAndLastPosition(vector<int> &arr, int n, int k)
+{
+    int first = firstOccurence(arr, k);
+    if (first == -1)
+    {
+        return {-1, -1};
+    }
+    int last = lastOccurence(arr, k);
+
+    return {first, last};
+}
