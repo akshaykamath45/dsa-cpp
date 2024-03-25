3e2a5a40f913d0b042a1e94ba9767123ff7331c9
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2024-03-25 20:56:15 +0530

    search in rotated sorted array 2

---
+++ binary search/bs on 1d arrays/searchInRotatedArray2.cpp
@@ -0,0 +1,49 @@
+class Solution
+{
+public:
+    bool search(vector<int> &arr, int target)
+    {
+        int n = arr.size();
+        int low = 0;
+        int high = n - 1;
+        while (low <= high)
+        {
+            int mid = (low + high) / 2;
+            if (arr[mid] == target)
+            {
+                return true;
+            }
+
+            // handling the edge case for duplicates,shrinking the search space
+            if (arr[low] == arr[mid] && arr[mid] == arr[high])
+            {
+                low++;
+                high--;
+                continue;
+            }
+            if (arr[low] <= arr[mid])
+            { // left half is sorted
+                if (arr[low] <= target && target <= arr[mid])
+                { // check if element is already there in sorted left half
+                    high = mid - 1;
+                }
+                else
+                {
+                    low = mid + 1; // search in unsorted right half
+                }
+            }
+            else
+            { // right half is sorted
+                if (arr[mid] <= target && target <= arr[high])
+                { // check if element is already present in sorted right half
+                    low = mid + 1;
+                }
+                else
+                {
+                    high = mid - 1; // search in unsorted left half
+                }
+            }
+        }
+        return false;
+    }
+};
