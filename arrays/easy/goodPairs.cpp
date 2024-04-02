df34793a9691ae5ecd2179499e18a36f103bc946
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2024-04-03 02:35:47 +0530

    number of good pairs

---
+++ arrays/easy/goodPairs.cpp
@@ -0,0 +1,21 @@
+// brute force
+class Solution
+{
+public:
+    int numIdenticalPairs(vector<int> &nums)
+    {
+        int count = 0;
+        int n = nums.size();
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = i; j < n; j++)
+            {
+                if (nums[i] == nums[j] && i < j)
+                {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+};
