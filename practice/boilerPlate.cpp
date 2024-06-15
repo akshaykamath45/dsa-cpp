0ae1d3ee534b15f407749513f4ce1a2b2e1a4c30
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2024-06-15 16:37:00 +0530

    updated boiler plate

--- practice/boilerPlate.cpp
+++ practice/boilerPlate.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
+void solve(vector<int> &arr)
+{
+    int n = arr.size();
+}
 int main()
 {
-    cout << "dsa practice";
+    vector<int> arr = {1, 2, 3, 4, 5};
+    cout << "before calling function " << endl;
+    for (auto i : arr)
+    {
+        cout << i << " ";
+    }
+    solve(arr);
+    cout << endl;
+    cout << "after calling function " << endl;
+    for (auto i : arr)
+    {
+        cout << i << " ";
+    }
     return 0;
 }
